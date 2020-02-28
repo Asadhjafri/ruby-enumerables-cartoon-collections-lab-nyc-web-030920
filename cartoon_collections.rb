f018bc68aa2c4ecb22ce1d0f55cf09cc65ac9034
Asad Jafri <asadhjafri@gmail.com>
2020-02-28 23:30:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,34 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = %w[
+Doc
+Dopey
+Bashful
+Grumpy
+Sneezy
+Sleepy
+Happy
+]
+
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+roll_call_dwarves(dwarves)
+
+planeteer_calls = %w["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |elements| elements.capitalize + '!'}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+summon_captain_planet(planeteer_calls)
+
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? { |elements| elements.length > 4}
 end
+
+long_planeteer_calls(planeteer_calls)
 
 def find_the_cheese# code an argument here
   # the array below is here to help
